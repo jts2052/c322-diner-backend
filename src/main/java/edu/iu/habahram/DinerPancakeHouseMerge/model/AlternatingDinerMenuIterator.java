387ae0cce33bc9290d01111bcbc1a1cc7bd00b29
Smith, Jacob Tyler <jts11@iu.edu>
2024-03-27 09:12:47 -0400

    In-class

---
+++ src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/model/AlternatingDinerMenuIterator.java
@@ -0,0 +1,35 @@
+package edu.iu.habahram.DinerPancakeHouseMerge.model;
+
+import java.util.Calendar;
+import java.util.Iterator;
+
+public class AlternatingDinerMenuIterator implements Iterator<MenuItem> {
+    MenuItem[] items;
+    int position;
+
+    public AlternatingDinerMenuIterator(MenuItem[] items) {
+        this.items = items;
+        Calendar date = Calendar.getInstance();
+        int day_of_week = date.get(Calendar.DAY_OF_WEEK);
+
+        if (day_of_week % 2 == 0) {
+            position = 0;
+        } else if (day_of_week % 3 == 0) {
+            position = 1;
+        } else {
+            position = 2;
+        }
+    }
+    public MenuItem next() {
+        MenuItem menuItem = items[position];
+        position = position + 3;
+        return menuItem;
+    }
+    public boolean hasNext() {
+        if (position >= items.length || items[position] == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
